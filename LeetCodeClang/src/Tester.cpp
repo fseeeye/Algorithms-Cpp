ac795655b8d12cd9f8eeef53b6c83259d7e118f4
fseeeye <fseeeye@gmail.com>
2021-12-22 17:29:54 +0800

    Add project files.

---
+++ LeetCodeClang/src/Tester.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+#include "greedy_algorithm/p455_assign_cookies.h"
+
+int main()
+{
+	Solution455 solution455;
+	solution455.test();
+
+	std::cin.get();
+}
---
+++ LeetCodeClang/src/greedy_algorithm/p455_assign_cookies.cpp
@@ -0,0 +1,20 @@
+﻿#include "p455_assign_cookies.h"
+
+#include <algorithm>
+
+int Solution455::findContentChildren(std::vector<int>& g, std::vector<int>& s)
+{
+	// sort g and s
+	std::sort(g.begin(), g.end());
+	std::sort(s.begin(), s.end());
+
+	int childPos = 0, cookiePos = 0;
+	while (childPos < g.size() && cookiePos < s.size())
+	{
+		// for current child find smallest cookie
+		if (s[cookiePos] >= g[childPos])
+			childPos++;
+		cookiePos++;
+	}
+	return childPos;
+}
