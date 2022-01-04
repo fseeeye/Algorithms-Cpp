50db30fb005c16c0ed82cabaa2d2cb3ac409c172
fseeeye <fseeeye@gmail.com>
2022-01-04 13:33:30 +0800

    refactor: update 76(minimum-window-substring) solution

--- LeetCodeClang/src/two_pointers/p76_minimum_window_substring/minimum_window_substring.cpp
+++ LeetCodeClang/src/two_pointers/p76_minimum_window_substring/minimum_window_substring.cpp
@@ -1,6 +1,7 @@
 ﻿#include "minimum_window_substring.h"
 
 #include <vector>
+#include <array>
 
 // Time:  O(N) = O(N) + O(N)
 // Space: O(1)
@@ -55,4 +56,50 @@ std::string Solution76::minWindow(std::string s, std::string t)
 	}
 
 	return min_window_size <= s_size ? s.substr(min_l, min_window_size) : "";
+}
+
+// Time:  O(N) = O(N) + O(N)
+// Space: O(1)
+// ref: https://leetcode-cn.com/problems/minimum-window-substring/solution/tong-su-qie-xiang-xi-de-miao-shu-hua-dong-chuang-k/
+std::string Solution76::minWindow_2(std::string s, std::string t)
+{
+	unsigned int s_size = s.size();
+	// 滑动窗口双指针
+	unsigned int l = 0, r = 0;
+	// 最小滑动窗口存储变量
+	std::array<unsigned int, 2> min = { s_size + 1, 0 };  // 最小滑动窗口的大小和l，方便最后取出该窗口
+	// 数组，目前 t 中每个字符在滑动窗口中的缺少数量，某个字符 > 0 说明还需要包含进来
+	// 用于判断滑动窗口是否包含了 T 中的所有元素
+	std::vector<int> chars_need(128, 0); 
+	// int，统计缺少的 t 中字符的总数量，需要同时维护 chars_need 和 chars_count
+	// 用于判断滑动窗口是否包含了 T 中的所有元素
+	unsigned int need_count = t.size();
+
+	for (char i : t)
+	{
+		++chars_need[i];
+	}
+
+	// step1: 不断移动 r 增大滑动窗口
+	for (; r < s_size; ++r)
+	{
+		if (chars_need[s[r]]-- > 0)
+		{
+			--need_count;
+		}
+		// step2: 如果滑动窗口已经包含了所有 t 中的元素，尝试右移 l
+		while (need_count == 0)
+		{
+			if (++chars_need[s[l]] > 0) {
+				// step3：如果滑动窗口移动到排除了必要元素，更新 min 并继续循环寻找新的满足条件的滑动窗口
+				++need_count;
+				
+				if (r - l + 1 < min[0])
+					min = { r - l + 1, l };
+			}
+			++l;
+		}
+	}
+
+	return min[0] <= s_size ? s.substr(min[1], min[0]) : "";
 }
--- LeetCodeClang/src/two_pointers/p76_minimum_window_substring/minimum_window_substring.h
+++ LeetCodeClang/src/two_pointers/p76_minimum_window_substring/minimum_window_substring.h
@@ -9,6 +9,7 @@
 class Solution76 {
 public:
 	std::string minWindow(std::string s, std::string t);
+	std::string minWindow_2(std::string s, std::string t);
 
 	void test() {
 		std::string rst1 = minWindow("ADOBECODEBANC", "ABC");
@@ -18,7 +19,16 @@ public:
 		ASSERT(rst2 == "a");
 
 		std::string rst3 = minWindow("a", "aa");
-		ASSERT(rst3 == "");
+		ASSERT(rst3.empty());
+
+		rst1 = minWindow_2("ADOBECODEBANC", "ABC");
+		ASSERT(rst1 == "BANC");
+
+		rst2 = minWindow_2("a", "a");
+		ASSERT(rst2 == "a");
+
+		rst3 = minWindow_2("a", "aa");
+		ASSERT(rst3.empty());
 
 		std::cout << "Solution 76 passed!\n";
 	}
