078596de63d1e2862b80f1b0b0e2891796019f3a
fseeeye <fseeeye@gmail.com>
2022-03-21 14:55:55 +0800

    feat(structure): add LRU solution

---
+++ LeetCodeClang/src/other_structures/p146_lru_cache/lru_cache.cpp
@@ -0,0 +1,42 @@
+﻿#include "lru_cache.h"
+
+LRUCache::LRUCache(int capacity)
+	: capacity(capacity)
+{
+}
+
+int LRUCache::get(int key)
+{
+	auto it = hash.find(key);
+	if (it == hash.end())
+		return -1;
+
+	auto& cache_it = it->second;
+	cache.splice(cache.begin(), cache, cache_it); // update cache and its iterator
+
+	return cache_it->second;
+}
+
+void LRUCache::put(int key, int value)
+{
+	auto it = hash.find(key);
+
+	if (it == hash.end()) // not found
+	{
+		// insert
+		cache.insert(cache.begin(), std::make_pair(key, value));
+		hash[key] = cache.begin();
+
+		// check capacity
+		if (cache.size() > capacity)
+		{
+			hash.erase(cache.back().first);
+			cache.pop_back();
+		}
+	}
+	else // found
+	{
+		it->second->second = value; // update value
+		cache.splice(cache.begin(), cache, it->second); // update cache and its iterator
+	}
+}
